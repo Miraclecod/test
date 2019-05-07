3302e58f1d73a6ca3a61dd2cc8bf4c41d0bddab8
Miraclecod <ser,mikelanjelo@yandex.ru>
2019-05-07 20:11:09 +0300

    hehe boy

---
+++ src/components/Example.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        <div>hello: {{ message }}</div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            message: null
+        }
+    },
+    created(){
+        this.message = this.$store.getters.getMessage;
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    $primarycolor: #333;
+    div{
+        color: $primarycolor;
+    }
+</style>
